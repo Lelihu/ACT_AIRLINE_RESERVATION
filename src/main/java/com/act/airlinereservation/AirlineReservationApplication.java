d307bff02c4feabbf5535f05980cbead1e44c1f3
Lelihu <lelihum@yahoo.com>
2021-09-18 12:37:32 -0700

    Update AirlineReservationApplication.java

--- src/main/java/com/act/airlinereservation/AirlineReservationApplication.java
+++ src/main/java/com/act/airlinereservation/AirlineReservationApplication.java
@@ -2,6 +2,11 @@ package com.act.airlinereservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.act.airlinereservation.domains.Schedule;
+import com.act.airlinereservation.repositories.ScheduleRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AirlineReservationApplication {
